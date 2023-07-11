b1558936a1706e0b3ec9f6e2a92eaf91b65d3db7
“derrick” <derrickmbugua50@gmail.com>
2023-07-11 14:03:18 +0300

    feat: get http using axios

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,24 @@
 <template>
-  <p :style="headingStyling">{{ firstName }} {{ lastName }}</p>
-  <p>{{ fullName }}</p>
-  <template v-for="item in items" :key="item.id">
-    <p v-if="item.price > 9999">{{ item.title }}</p>
-  </template>
+ <PostList />
 </template>
 
 <script>
+
+import PostList from './components/PostList.vue'
+
 export default {
   name: "App",
   data() {
     return {
-      firstName: "D",
-      lastName: "M",
-      headingStyling: {
-        color: "orange",
-      },
-      items: [
-        {
-          id: 1,
-          title: "TV",
-          price: 10000,
-        },
-        {
-          id: 2,
-          title: "IPhone",
-          price: 1000,
-        },
-        {
-          id: 3,
-          title: 'Watch',
-          price: 100
-        }
-      ],
+     
     };
   },
   methods: {},
   computed: {
-    fullName() {
-      return this.firstName + " " + this.lastName;
-    },
   },
+  components:{
+    PostList
+  }
 };
 </script>
 
@@ -49,32 +27,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-abel {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-input[type="text"],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
+
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="getPosts">Load Posts</button>
+  </div>
+  <p v-if="errorMsg">{{ errorMsg }}</p>
+  <template v-for="post in posts" :key="post.id"> 
+    <p>{{ post.title }}</p>
+  </template>
+</template>
+<script>
+import axios from 'axios'
+export default {
+
+  name: 'PostList',
+  data(){
+    return{
+      posts: [],
+      errorMsg: ''
+    }
+  },
+  methods: {
+    getPosts(){
+      axios.get("https://jsonplaceholder.typicode.com/postssd")
+      .then((response) => {
+        console.log(response.data)
+        this.posts = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+        this.errorMsg = error.message
+      })
+    }
+  }
+}
+</script>
+<style>
+  
+</style>

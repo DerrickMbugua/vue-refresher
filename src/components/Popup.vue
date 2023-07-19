f5f8dd9c888fd9706c8bfc1da81548bc18e14c37
“derrick” <derrickmbugua50@gmail.com>
2023-07-19 12:51:52 +0300

    feat: validating emits events

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <h2>Popup Component</h2>
-    <button @click="$emit('close','Sparta')">Close Popup</button>
+    <input type="text" v-model="name"/>
+    <button @click="$emit('close',name)">Close Popup</button>
   </div>
 </template>
 <script>
 export default {
   data(){
     return{
-     
+     name: ''
     }
   },
-  emits: ["close"]
+  emits: {
+    close(name) {
+      if(!name){
+        return false;
+      }else{
+        return true;
+      }
+    } // event to emit when closing the popup with a value passed as parameter
+  }
 }
 </script>
 <style>

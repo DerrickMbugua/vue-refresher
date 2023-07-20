79e0735f73e9950923c8d2a07d4582eb422ab3c0
“derrick” <derrickmbugua50@gmail.com>
2023-07-20 10:57:19 +0300

    feat: component styles scoped

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,15 @@
 <template>
-  <!-- <div>
-    <h1>App Component</h1>
-    <Card></Card>
-    <Card>Mapema ndo Best</Card>
-    <Card><h2>Mapema ndo Best</h2></Card>
-  </div> -->
-  <Card>
-    <template v-slot:header>
-      <h3>Header</h3>
-       </template>
-       <template v-slot:default>
-      <h3>Default</h3>
-       </template>
-       <template v-slot:footer>
-      <h3>Footer</h3>
-       </template>
-  </Card>
-
-  <List>
-    <template v-slot:default="slotProps">
-      <h3>{{ slotProps.firstName }} {{ slotProps.lastName }}</h3>
-      <h3>{{ slotProps.lastName }} {{ slotProps.firstName }} </h3>
-    </template>
-  </List>
+  <p>App Component</p>
+ <Header/>
 </template>
 
 <script>
-import Card from "./components/Card.vue";
-import List from "./components/List.vue";
+import Header from './components/Header.vue';
+
 export default {
   name: "App",
   components: {
-    Card,
-    List
+    Header
   },
   data() {
     return {};
@@ -44,7 +21,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -53,28 +30,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-abel {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
+p {
+  color: orange;
 }
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-input[type="text"],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Header Component</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  p{
+    color: red;
+  }
+</style>

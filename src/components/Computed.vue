8162ce84c98592cf203753cc218270a97a3d304e
“derrick” <derrickmbugua50@gmail.com>
2023-07-27 10:57:19 +0300

    feat: replacing computed properties with reactive

--- src/components/Computed.vue
+++ src/components/Computed.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
     <h2>{{ fullName }}</h2>
+    <h2>{{ reactiveFullName }}</h2>
   </div>
 </template>
 <script>
-import {ref, computed} from 'vue';
+import { ref, computed, reactive, toRefs } from "vue";
 export default {
-  name: 'Computed',
-  setup(){
-    const firstName = ref('D');
-    const lastName = ref('M');
+  name: "Computed",
+  setup() {
+    const firstName = ref("D");
+    const lastName = ref("M");
 
-    const fullName = computed(function(){
+    const fullName = computed(function () {
       return firstName.value + lastName.value;
     });
-    return{
-      firstName, lastName, fullName
-    }
-  }
 
-}
+    const state = reactive({
+      reactiveFirstName: "Sparta",
+      reactiveLastName: "Bender",
+    });
+
+    const reactiveFullName = computed(function(){
+      return state.reactiveFirstName + state.reactiveLastName
+    });
+
+   
+    return {
+      firstName,
+      lastName,
+      fullName,
+      ...toRefs(state),
+      reactiveFullName
+    };
+  },
+};
 </script>
 <style>
-  
 </style>

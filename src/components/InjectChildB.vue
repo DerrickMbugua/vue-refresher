381ab5b99aeb6f5077c04d03f8a09bea862cf72f
“derrick” <derrickmbugua50@gmail.com>
2023-07-29 10:38:55 +0300

    feat: provide and inject with ref

--- src/components/InjectChildB.vue
+++ src/components/InjectChildB.vue
@@ -3,14 +3,21 @@
     <h2>Child B</h2>
     <p>Username is {{ username }}</p>
     <p>Age is {{ myAge }}</p>
+    <p>COmposition inject age is {{ age }}</p>
   </div>
 </template>
 <script>
+import { inject } from "vue";
 export default {
-  name: 'ChildB',
-  inject: ["username", "myAge"]
-}
+  name: "ChildB",
+  inject: ["username", "myAge"],
+  setup() {
+    const age = inject("myAge");
+    return {
+      age,
+    };
+  },
+};
 </script>
 <style lang="">
-  
 </style>

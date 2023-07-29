7c0e60d6a08f4e6563458ed8f47562e43522e6c5
“derrick” <derrickmbugua50@gmail.com>
2023-07-29 10:33:22 +0300

    feat: provide and inject with ref

--- src/components/Watchers.vue
+++ src/components/Watchers.vue
@@ -1,53 +1,65 @@
 <template>
   <div>
-    <button @click="count++">Increment {{ count }}</button>
     <button @click="increment">Increment {{ counter }}</button>
   </div>
   <div>
     <input type="text" v-model="firstName" />
   </div>
+  <div>
+    <input type="text" v-model="lastName" />
+    <input type="number" v-model="age" />
+  </div>
 </template>
 <script>
-import { ref, watch } from 'vue';
+import { ref, watch, reactive, toRefs } from "vue";
 
 export default {
-  name: 'Watchers',
-  data() {
-    return{
-      count : 0,
-    }
-  },
-  watch:{
-    count(newValue,oldValue){
-      if(newValue === 5){
-        alert("High Volume");
-      }
-    }
-  },
-  setup(){
+  name: "Watchers",
+  setup() {
     const counter = ref(0);
-    const firstName = ref('');
+    const firstName = ref("");
 
-    function increment(){
-      counter.value++
+    function increment() {
+      counter.value++;
     }
+    watch(
+      [counter, firstName],
+      (newValues, oldValues) => {
+        // console.log("Counter new value", newValues[0]);
+        // console.log("Counter old value", oldValues[0]);
+        // console.log("Firstname new value", newValues[1]);
+        // console.log("Firstname old value", oldValues[1]);
+      },
+      {
+        immediate: true,
+      }
+    );
 
-    watch([counter, firstName], (newValues, oldValues) => {
-     console.log("Counter new value", newValues[0]);
-     console.log("Counter old value", oldValues[0]);
-     console.log("Firstname new value", newValues[1]);
-     console.log("Firstname old value", oldValues[1]);
-    },{
-      immediate: true
-    })
-    
-    return{
-      counter,increment,firstName
-    }
-  }
- 
-}
+    const state = reactive({
+      lastName: "Doe",
+      age: 25,
+    });
+
+    watch(
+      () => {
+        return { ...state };
+      },
+      function (newValues, oldValues) {
+        console.log("Lastname new value", newValues.lastName);
+        console.log("Lastname old value", oldValues.lastName);
+        console.log("Age new value", newValues.age);
+        console.log("Age old value", oldValues.age);
+      }
+    );
+
+    return {
+      counter,
+      increment,
+      firstName,
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 <style scoped>
-  
 </style>
